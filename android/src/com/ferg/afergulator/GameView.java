443032e8c9ebbf5eefdd2ff01b3cc531ca9abe03
Nick Fedesna <n.fedesna@gmail.com>
2016-05-16 10:33:47 -0500

    WIP

--- android/src/com/ferg/afergulator/GameView.java
+++ android/src/com/ferg/afergulator/GameView.java
@@ -7,8 +7,7 @@ import android.os.Environment;
 import android.util.AttributeSet;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.nio.*;
 import java.util.Arrays;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -22,7 +21,7 @@ import static android.opengl.GLES20.*;
 public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
 
     // @formatter:off
-    private static final String VERT_SHADER_SRC =
+    static final String VERT_SHADER_SRC =
             "attribute vec4 vPosition;" +
             "attribute vec2 vTexCoord;" +
             "varying vec2 texCoord;" +
@@ -31,7 +30,7 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
             "    gl_Position = vec4((vPosition.xy * 2.0) - 1.0, vPosition.zw);" +
             "}";
 
-    private static final String FRAG_SHADER_SRC =
+    static final String FRAG_SHADER_SRC =
             "precision highp float;" +
             "varying vec2 texCoord;" +
             "uniform sampler2D texture;" +
@@ -42,9 +41,10 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
             "    i = i - ((i / 64) * 64);" +
             "    vec3 color = vec3(palette[i]) / 256.0;" +
             "    gl_FragColor = vec4(color, 1);" +
+            "    gl_FragColor = vec4(1, 0, 0, 1);" +
             "}";
 
-    private static final int[] NES_PALLETE = {
+    static final int[] NES_PALLETE = {
             0x666666, 0x002A88, 0x1412A7, 0x3B00A4, 0x5C007E,
             0x6E0040, 0x6C0600, 0x561D00, 0x333500, 0x0B4800,
             0x005200, 0x004F08, 0x00404D, 0x000000, 0x000000,
@@ -61,8 +61,16 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
 
     // @formatter:on
 
+    static final int BYTES_PER_FLOAT = Float.SIZE / 8;
+    static final int STRIDE_BYTES = 2 * BYTES_PER_FLOAT;
+
     private int mProgram;
     private int mTexture;
+    private int mVertexHandle;
+    private int mTexCoordHandle;
+
+    private ByteBuffer mVertexBuffer;
+    private ByteBuffer mTexCoordBuffer;
 
     public GameView(Context context) {
         super(context);
@@ -91,10 +99,8 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
         byte[] rom = ByteStreams.toByteArray(is);
         byte[] start = Arrays.copyOfRange(rom, 0, 3);
         Timber.d("%s ROM: %s (%dk)", new String(start), name, rom.length / 1024);
-        boolean result = Nesdroid.LoadRom(rom, name);
-        Timber.d("%s, loaded = %B", name, result);
 
-        return result;
+        return Nesdroid.LoadRom(rom, name);
     }
 
     private void setupContextPreserve() {
@@ -113,17 +119,19 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        Timber.i("Engine.init()...");
+        Timber.i("onSurfaceCreated(%s, %s)...", gl.glGetString(GL_VERSION), config);
+
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glEnable(GL_CULL_FACE);
         glEnable(GL_DEPTH_TEST);
 
         mProgram = createProgram(VERT_SHADER_SRC, FRAG_SHADER_SRC);
 
-        int posAttrib = glGetAttribLocation(mProgram, "vPosition");
-        int texCoordAttr = glGetAttribLocation(mProgram, "vTexCoord");
+        mVertexHandle = glGetAttribLocation(mProgram, "vPosition");
+        mTexCoordHandle = glGetAttribLocation(mProgram, "vTexCoord");
+
         int paletteLoc = glGetUniformLocation(mProgram, "palette");
-        int textureUni = glGetUniformLocation(mProgram, "texture");
+        int textureUni = glGetUniformLocation(mProgram, "texture"); // TODO connect to Go buffer?
 
         int[] textures = new int[1];
         mTexture = textures[0];
@@ -135,32 +143,69 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
         gl.glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         glUseProgram(mProgram);
-        glEnableVertexAttribArray(posAttrib);
-        glEnableVertexAttribArray(texCoordAttr);
+
+        glEnableVertexAttribArray(mVertexHandle);
+        glEnableVertexAttribArray(mTexCoordHandle);
 
         glUniform1iv(paletteLoc, NES_PALLETE.length, NES_PALLETE, 0);
 
         int[] buffers = new int[2];
         glGenBuffers(2, buffers, 0);
 
+        float[] vertices = new float[]{-1, 1, -1, -1, 1, -1, 1, -1, 1, 1, -1, 1};
+        float[] textureCoords = new float[]{0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1};
+
         glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
-        ByteBuffer vertBuffer = ByteBuffer.allocateDirect(12);
-        vertBuffer.order(ByteOrder.nativeOrder());
-        vertBuffer.put(new byte[]{-1, 1, -1, -1, 1, -1, 1, -1, 1, 1, -1, 1});
-        vertBuffer.position(0);
-        glBufferData(GL_ARRAY_BUFFER, 12, vertBuffer, GL_STATIC_DRAW);
+        mVertexBuffer = ByteBuffer.allocateDirect(vertices.length * BYTES_PER_FLOAT);
+        mVertexBuffer.order(ByteOrder.nativeOrder()).asFloatBuffer().put(vertices).position(0);
+        glBufferData(GL_ARRAY_BUFFER, vertices.length * BYTES_PER_FLOAT, mVertexBuffer, GL_STATIC_DRAW);
 
         glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
-        ByteBuffer textureCoordBuffer = ByteBuffer.allocateDirect(12);
-        textureCoordBuffer.order(ByteOrder.nativeOrder());
-        textureCoordBuffer.put(new byte[]{0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1});
-        textureCoordBuffer.position(0);
-        glBufferData(GL_ARRAY_BUFFER, 12, textureCoordBuffer, GL_STATIC_DRAW);
+        mTexCoordBuffer = ByteBuffer.allocateDirect(textureCoords.length * BYTES_PER_FLOAT);
+        mTexCoordBuffer.order(ByteOrder.nativeOrder()).asFloatBuffer().put(textureCoords).position(0);
+        glBufferData(GL_ARRAY_BUFFER, textureCoords.length * BYTES_PER_FLOAT, mTexCoordBuffer, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(posAttrib, 2, GL_BYTE, false, 0, vertBuffer);
-        glVertexAttribPointer(texCoordAttr, 2, GL_BYTE, false, 0, textureCoordBuffer);
+//        glVertexAttribPointer(mVertexHandle, 2, GL_FLOAT, false, 2, mVertexBuffer);
+//        glEnableVertexAttribArray(mVertexHandle);
+
+//        glVertexAttribPointer(mTexCoordHandle, 2, GL_FLOAT, false, 2, mTexCoordBuffer);
+//        glEnableVertexAttribArray(mTexCoordHandle);
 
         Timber.d("Surface Created…");
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        byte[] frame = Nesdroid.GetPixelBuffer();
+        if (frame != null) {
+            Timber.d("frame: %,d bytes", frame.length);
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glUseProgram(mProgram);
+
+            glEnableVertexAttribArray(mVertexHandle);
+            glVertexAttribPointer(mVertexHandle, 2, GL_FLOAT, false, 2 * BYTES_PER_FLOAT, mVertexBuffer);
+
+            glEnableVertexAttribArray(mTexCoordHandle);
+            glVertexAttribPointer(mTexCoordHandle, 2, GL_FLOAT, false, 2 * BYTES_PER_FLOAT, mTexCoordBuffer);
+
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, mTexture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 256, 256, 0, GL_RGBA,
+                         GL_UNSIGNED_BYTE, ByteBuffer.wrap(frame));
+
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
+            glDisableVertexAttribArray(mVertexHandle);
+            glDisableVertexAttribArray(mTexCoordHandle);
+        }
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        Timber.i("Surface Changed (%d, %d)…", width, height);
+        glViewport(0, 0, width, height);
     }
 
     private int createProgram(String vertShaderSrc, String fragShaderSrc) {
@@ -181,30 +226,6 @@ public class GameView extends GLSurfaceView implements GLSurfaceView.Renderer {
         glShaderSource(shader, shaderCode);
         glCompileShader(shader);
         return shader;
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        Timber.i("Surface Changed (%d, %d)…", width, height);
-        glViewport(0, 0, width, height);
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        byte[] frame = Nesdroid.GetPixelBuffer();
-        if (frame != null) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            glUseProgram(mProgram);
-
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, mTexture);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 256, 256, 0, GL_RGBA,
-                         GL_UNSIGNED_SHORT_4_4_4_4,
-                         ByteBuffer.wrap(frame));
-
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        }
     }
 
 }
